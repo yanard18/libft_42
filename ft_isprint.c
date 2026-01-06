ff95c28e5192955f4028dae9e64a936a6d006492
Deniz Yanar <yanard18@gmail.com>
2026-01-06 16:27:24 +0300

    add testing to ft_isprint.c

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,18 +6,26 @@ int	ft_isprint(char c)
 	return (c >= 32 && c <= 126);
 }
 
-void	test(char c)
+void	test(void)
 {
-	printf("char: %c(%d), ft_isprint: %d, isprint: %d\n", c,c, ft_isprint(c), isprint(c));
+	int	c;
+
+	c = 0;
+	while (c < 256)
+	{
+		if ((ft_isprint(c) > 0 && isprint(c) == 0) || ft_isprint(c) == 0 && isprint(c) > 0)
+		{
+			printf("[ERROR] char: %d, ft_isprint: %d, isprint: %d\n", c, ft_isprint(c), isprint(c));
+			break ;
+		}
+		printf("[PASSED] char: %d, ft_isprint: %d, isprint: %d\n", c, ft_isprint(c), isprint(c));
+		c++;
+	}
 }
 
 int	main(void)
 {
-	test(31);
-	test(32);
-	test(33);
-	test(126);
-	test(127);
+	test();
 	return (0);
 }
 
