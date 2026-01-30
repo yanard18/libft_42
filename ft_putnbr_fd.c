65101e52eb01d87852506f200533f7500bae416e
deniz <yanard18@gmail.com>
2026-01-30 18:19:42 +0300

    fix formatting ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,33 +1,31 @@
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-    if (n == -2147483648)
-    {
-        ft_putstr_fd("-2147483648", fd);
-    }
-    else
-    {
-        if (n < 0)
-        {
-            n = -n;
-            ft_putchar_fd('-', fd);
-            ft_putnbr_fd(n, fd);
-        }
-        if (n <= 9)
-        {
-            ft_putchar_fd(n + '0', fd);
-        }
-        if (n >= 9) 
-        {
-            ft_putnbr_fd(n / 10, fd);
-            ft_putnbr_fd(n % 10, fd);
-        }
-    }
-    
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+	{
+		if (n < 0)
+		{
+			n = -n;
+			ft_putchar_fd('-', fd);
+			ft_putnbr_fd(n, fd);
+		}
+		if (n <= 9)
+		{
+			ft_putchar_fd(n + '0', fd);
+		}
+		if (n >= 9) 
+		{
+			ft_putnbr_fd(n / 10, fd);
+			ft_putnbr_fd(n % 10, fd);
+		}
+	}
+
 }
 
-int main()
+int	main()
 {
-    ft_putnbr_fd(2147483647, 1);
+	ft_putnbr_fd(2147483647, 1);
 }
