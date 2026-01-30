afa13d2ce3f74b76d62b50ce12285a8396588acb
deniz <yanard18@gmail.com>
2026-01-30 18:16:04 +0300

    fix ft_putstr_fd.c

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,12 +1,10 @@
 #include "libft.h"
 
-void ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-    int i;
+    unsigned long	i;
 
     i = 0;
     while(s[i])
-    {
         ft_putchar_fd(s[i++], fd);
-    }
 }
